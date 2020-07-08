2ef88c494c36ea585505cdacc8b14d13ae6f2fd1
tianqihenghaola <15102737155@163.com>
2020-07-08 14:13:34 +0800

    增加login

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+// 主容器组件
 import Home from '../layout';
+
+// 登录
+import Login from '@/views/login';
+
 import CeShi from '../views/ceshi';
 
 Vue.use(Router);
 
 let routes = [
     {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        beforEnter: (to, from, next) => {
+            next('/login')
+        },
+    },
+    {
         path: '/',
-        redirect: '/home/CeShi'
+        redirect: '/home/login'
     },
     {
         path: '/home',
         name: 'home',
         component: Home,
         children: [
-            {path: '/home/CeShi', component: CeShi}
+            {path: '/home/ceShi', component: CeShi}
         ]
     }
 ];
